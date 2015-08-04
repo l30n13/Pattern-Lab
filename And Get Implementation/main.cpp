55e4beea5fc2ab7b87945754906b7ed48e6d86eb
l30n13 <mrtonmoy12@gmail.com>
2015-08-05 02:13:04 +0600

    Updated

--- And Get Implementation/main.cpp
+++ And Get Implementation/main.cpp
@@ -4,27 +4,32 @@ using namespace std;
 
 int main()
 {
-    int i=4,x[4][3],y, b = 1, w[2]={0,0};
+    int i=4;
     int w_old[3]={0,0,0}, w_new[3];
+    int x[3], y, b = 1;
 
     while(i--){
-        cout<<"x1 x2\n";
-        for(int i =0; i < 4; i++){
-            for(int j = 0; j < 2;j++){
-            cin>>x[i][j];
-            }
-            x[i][2] = b;
+        for(int i = 0; i < 2; i++){
+            cout<<"x "<<i + 1<<" : ";
+            cin>>x[i];
         }
 
-        for(int j = 0; j < 2; j++){
-            for(int i = 3; i >= 0; i--){
-                w[j] -= x[i][j];
-            }
+        x[2] = b;
+
+        if(x[0] == -1 && x[1] == -1){
+            y = -1;
+        }else if(x[0] == -1 && x[1] == 1){
+            y = -1;
+        }else if(x[0] == 1 && x[1] == -1){
+            y = -1;
+        }else{
+            y = 1;
         }
 
         for(int i = 0;i < 3;i++){
             w_new[i] = x[i] * y;
         }
+
         for(int i = 0;i < 3;i++){
             w_new[i] = w_old[i] + w_new[i];
             w_old[i] = w_new[i];
